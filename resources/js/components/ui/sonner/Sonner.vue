4a92a5317ba3479c106d6ae630a174afb1ae86c8
Aleksandr Manukyan <alikmanukian@gmail.com>
2025-12-28 18:02:55 +0400

    add packages toast, debugbar

---
+++ resources/js/components/ui/sonner/Sonner.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Toaster as Sonner, type ToasterProps } from 'vue-sonner'
+
+const props = defineProps<ToasterProps>()
+</script>
+
+<template>
+    <Sonner
+        class="toaster group"
+        :toast-options="{
+            classes: {
+                toast: 'group toast group-[.toaster]:bg-white group-[.toaster]:text-gray-900 group-[.toaster]:border-gray-200 group-[.toaster]:shadow-lg',
+                description: 'group-[.toast]:text-gray-500',
+                actionButton: 'group-[.toast]:bg-shop-blue group-[.toast]:text-white',
+                cancelButton: 'group-[.toast]:bg-gray-100 group-[.toast]:text-gray-500',
+            },
+        }"
+        v-bind="props"
+    />
+</template>
